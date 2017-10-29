9d76d4a3fc07e6441ef65e00d04b5025ae1b39fc
fornesarturo <fornesarturo@gmail.com>
2017-10-29 14:57:09 -0600

    Attack and Spawners

---
+++ Assets/Scripts/Spawners/JellySpawnerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellySpawnerScript : MonoBehaviour {
+
+	public GameObject spawnOfEvil;
+	private int count = 10;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawn ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawSphere(transform.position, 1f);
+	}
+
+	IEnumerator spawn() {
+		while (count > 0) {
+			yield return new WaitForSeconds (3f);
+			Instantiate (spawnOfEvil, transform.position, transform.rotation);
+			count--;
+		}
+		yield break;
+	}
+}
