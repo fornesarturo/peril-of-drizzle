e28df5087081ae74d8bbaab49769fc2f61315f01
fornesarturo <fornesarturo@gmail.com>
2017-10-30 11:14:35 -0600

    Camera + Movement + Attack

--- Assets/Scripts/Enemy/FlameController.cs
+++ Assets/Scripts/Enemy/FlameController.cs
@@ -6,7 +6,7 @@ using System;
 
 public class FlameController : MonoBehaviour {
 
-	public GameObject[] players = new GameObject[4];
+	public GameObject[] players;
 	private static int speed = 5;
 	private static int life = 3;
 	private GameObject closest;
@@ -22,55 +22,77 @@ public class FlameController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 		players = GameObject.FindGameObjectsWithTag("PlayerTag");
-        float maxDistance = players.Min(x => (x.transform.position - this.transform.position).sqrMagnitude);
-		GameObject closest = players.First(x => (x.transform.position - this.transform.position).sqrMagnitude == maxDistance);
+		if (players.Length > 0) {
+			float maxDistance = players.Min(x => (x.transform.position - this.transform.position).sqrMagnitude);
+			GameObject closest = players.First(x => (x.transform.position - this.transform.position).sqrMagnitude == maxDistance);
 
-        if(closest != prevClosest) {
-            prevClosest = closest;
-        }
-		isFar = Vector2.Distance (transform.position, prevClosest.transform.position) > 1f;
-        float direction = (transform.position - prevClosest.transform.position).x;
-        if (isFar) {
-            animator.SetFloat("Speed", -direction);
-            transform.position = Vector2.MoveTowards(transform.position, prevClosest.transform.position, speed * Time.deltaTime);
-        }
-        else {
-            animator.SetFloat("Speed", -direction);
-            if (!attackActive) {
-                StartCoroutine(attackCorroutine(closest));
-                attackActive = true;
-            }
-        }
+			if(closest != prevClosest) {
+				prevClosest = closest;
+			}
+			isFar = Vector2.Distance (transform.position, prevClosest.transform.position) > 1f;
+			float direction = (transform.position - prevClosest.transform.position).x;
+			if (isFar) {
+				animator.SetFloat("Speed", -direction);
+				transform.position = Vector2.MoveTowards(transform.position, prevClosest.transform.position, speed * Time.deltaTime);
+			}
+			else {
+				animator.SetFloat("Speed", -direction);
+				if (!attackActive) {
+					StartCoroutine(attackCorroutine(closest));
+					attackActive = true;
+				}
+			}
+		}
     }
 		
 	void OnTriggerEnter2D(Collider2D c) {
-		Debug.Log ("Collision");
 		switch (c.transform.tag) {
 		case "Bullet":
 			Destroy (c.transform.gameObject);
 			life--;
 			if (life <= 0) {
 				Destroy (transform.gameObject);
+				break;
 			}
+			StartCoroutine(hitCorroutine (0.1f));
+			break;
+		case "Melee":
+			Destroy (c.transform.gameObject);
+			life--;
+			if (life <= 0) {
+				Destroy (transform.gameObject);
+				break;
+			}
+			StartCoroutine(hitCorroutine (0.2f));
 			break;
 		}
 	}
 
 	void Attack(GameObject go) {
-		Debug.Log ("Attack!" + Time.time);
-		PlayerController pc = go.GetComponent<PlayerController> ();
-		pc.life--;
+		if (go != null) {
+			PlayerController pc = go.GetComponent<PlayerController> ();
+			if (pc != null) {
+				pc.life = pc.life - 1;
+			}
+		}
 	}
 
 	IEnumerator attackCorroutine(GameObject go) {
 		while (true) {
-			yield return new WaitForSeconds(1f);
             animator.SetTrigger("Attack");
             Attack (go);
 			if (isFar) {
 				attackActive = false;
 				yield break;
 			}
+			yield return new WaitForSeconds(1f);
 		}
+	}
+
+	IEnumerator hitCorroutine(float seconds) {
+		speed = 0;
+		yield return new WaitForSeconds (seconds);
+		speed = 5;
+		yield break;
 	}
 }
